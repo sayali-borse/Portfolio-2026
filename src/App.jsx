7ff47f56c9258b156d108e69a20a333481d62e6e
sayali-borse <54106196+sayali-borse@users.noreply.github.com>
2026-03-20 13:54:08 +0530

    Create basic Theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="min-h-screen overflow-x-hidden">
+      <Navbar />
+      <Main />
+    </div>
+  );
 };
 
 export default App;
