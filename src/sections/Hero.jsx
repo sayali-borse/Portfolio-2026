4c9daeb681cdb4a6d50d561cf747f7d67969e16b
sayali-borse <54106196+sayali-borse@users.noreply.github.com>
2026-03-21 14:39:44 +0530

    Update and CSS and create hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -15,11 +15,12 @@ const Hero = () => {
       </div>
       {/*  green dot */}
       <div className="absolute inset-0 overflow-hidden pointer-events-none">
-        {[...Array(80)].map((_, i) => (
+        {[...Array(70)].map((_, i) => (
           <div
-            className="absolute w-1.5 h-1.5 rounded-full opacity-60"
+            className="absolute w-0.75 h-0.75 rounded-full opacity-60"
             style={{
-              backgroundColor: " #20b2a6",
+              backgroundColor: "#20b2a6",
+              // backgroundColor: "#ff4d6d",
               left: `${Math.random() * 100}%`,
               top: `${Math.random() * 100}%`,
               animation: `slow-drift ${30 + Math.random() * 20}s ease-in-out infinite`,
@@ -30,6 +31,21 @@ const Hero = () => {
       </div>
 
       {/* ---- content of Hero section---- */}
+      <div>
+        <div>
+          {/* ---left section  */}
+
+          <div>
+            <div className="animate-fade-in">
+              <span className=" w-2 h-2 bg-primary rounded-full">
+                {" "}
+                Full-Stack Developer | React-Specialist
+              </span>
+            </div>
+          </div>
+          {/* --- right section  */}
+        </div>
+      </div>
     </section>
   );
 };
