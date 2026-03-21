b2f92d6983bd7929479fcc9fb7de551c8d5bd131
sayali-borse <54106196+sayali-borse@users.noreply.github.com>
2026-03-21 13:59:24 +0530

    create a animation on Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
 const Hero = () => {
-  return <section>Hero</section>;
+  return (
+    <section className="relative min-h-screen flex items-center overflow-hidden ">
+      {/*    Bg- Background Image */}
+
+      <div className="absolute inset-0">
+        <img
+          src="./public/hero-bg.jpg"
+          alt="Hero image"
+          className="w-full h-full object-cover opacity-40"
+        />
+        <div className="absolute inset-0 bg-linear-to-b from-background/20 via-background/80 to-background" />
+      </div>
+      {/*  green dot */}
+      <div className="absolute inset-0 overflow-hidden pointer-events-none">
+        {[...Array(80)].map((_, i) => (
+          <div
+            className="absolute w-1.5 h-1.5 rounded-full opacity-60"
+            style={{
+              backgroundColor: " #20b2a6",
+              left: `${Math.random() * 100}%`,
+              top: `${Math.random() * 100}%`,
+              animation: `slow-drift ${30 + Math.random() * 20}s ease-in-out infinite`,
+              animationDelay: `${Math.random() * 5}s`,
+            }}
+          />
+        ))}
+      </div>
+
+      {/* ---- content of Hero section---- */}
+    </section>
+  );
 };
 
 export default Hero;
